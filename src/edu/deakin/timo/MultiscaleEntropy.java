44a8397252b4e36cb831eef70468cd8320bf9c88
Timo Rantalainen <tjrantal@gmail.com>
2018-01-31 10:19:54 +0200

    Moved common functions to BaseClass

--- src/edu/deakin/timo/MultiscaleEntropy.java
+++ src/edu/deakin/timo/MultiscaleEntropy.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 	@author Timo Rantalainen	
 */
 
-public class MultiscaleEntropy{
+public class MultiscaleEntropy extends BaseClass{
 	private double[][] mseResults;	/*<Array for MSE results, accessed with @link #getMSE() after construction*/
 	private double[] dataIn;		/*<Array to contain the signal in*/
 	private double r;				/*<Relative tolerance*/
@@ -82,21 +82,6 @@ public class MultiscaleEntropy{
 		return ret;
 	}
 
-	
-	/**Standard deviation
-		This method of calculating sd explained in e.g. http://www.johndcook.com/blog/2008/09/26/comparing-three-methods-of-computing-standard-deviation/
-		
-		@param	arr	array for which the sd is to be calculated
-		@return standard deviation
-	*/
-	private double std(double[] arr){
-		double sum = 0,sum2 = 0;
-		for (int i = 0; i<arr.length;++i){
-			sum+=arr[i];
-			sum2+=arr[i]*arr[i];
-		}
-		return Math.sqrt((sum2-sum*sum/((double) arr.length))/(((double) arr.length)-1d));		
-	}
 	
 	/**A subclass to enable multi threading different coarseness level SE calculations*/
 	public class SERunnable implements Runnable{
@@ -178,23 +163,6 @@ public class MultiscaleEntropy{
 				}
 			}
 			return se;
-		}
-
-		/**Create coarse-grained time series
-			@param arr			original time series in
-			@param scaleFactor	the length of mean for the coarse-grained time series
-			@return				Coarse-grained time series
-		*/
-		private double[] coarseGraining(double[] arr, int scaleFactor){
-			double[] y = new double[arr.length/scaleFactor];
-			for (int i = 0; i<arr.length/scaleFactor;++i){
-				y[i] = 0;
-				for (int k=0;k<scaleFactor;++k){
-					y[i] += arr[i*scaleFactor+k];
-				}
-				y[i] /= (double) scaleFactor;
-			}
-			return y;
 		}
 	}
 
--- src/edu/deakin/timo/package-info.java
+++ src/edu/deakin/timo/package-info.java
@@ -1,4 +1,5 @@
 /**
 	Package for calculating multiscale entropy. To be used with Octave/Matlab.
+	Include's Costa's () multiscale entorpy (MSE) and Ihlen's refined composite multiscale entropy (RCME), and refined multiscale permutation entropy (RMPE).
 */
 package edu.deakin.timo;
